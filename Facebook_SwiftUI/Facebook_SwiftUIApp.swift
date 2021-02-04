ada7e3ed3104d57beadc9d43a269c316059afdd4
Vandan Patel <vandan.ets@gmail.com>
2021-02-03 23:37:13 -0500

    Initial Commit

---
+++ Facebook_SwiftUI/Facebook_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_SwiftUIApp.swift
+//  Facebook_SwiftUI
+//
+//  Created by Vandan Patel on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
